db7388cf15159c0163ae7843184ecfe13e5e1e20
AlicjaFranczak <alicja.franczak2@gmail.com>
2023-04-22 14:41:02 +0200

    zadania22_04

--- Zadania22.04/src/main/java/org/example/App.java
+++ Zadania22.04/src/main/java/org/example/App.java
@@ -1,13 +1,79 @@
 package org.example;
-
+import java.util.Set;
+import java.util.HashSet;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        //Stwórz dwa HashSety integerów i zaimplementuj dla nich następujące działania rachunków zbiorów:
+        // suma, część wspólna, różnica, suma bez części wspólnej.
+        // Wynik działania powinien trafić do trzeciego zbioru.
+
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(13);
+        numbers.add(22);
+        numbers.add(-34);
+        numbers.add(10000);
+
+        HashSet<Integer> numbers2 = new HashSet<>();
+        numbers2.add(13);
+        numbers2.add(22);
+        numbers2.add(-45);
+        numbers2.add(1500);
+
+        HashSet<Integer> sum = new HashSet<>();
+
+        for (int number : numbers) {
+            sum.add(number);
+        }
+        for (int number2 : numbers2) {
+            sum.add(number2);
+        }
+        System.out.println(sum);
+// A-B
+        HashSet<Integer> sum1 = new HashSet<>();
+
+        for (int number : numbers) {
+            sum1.add(number);
+        }
+        for (int number2 : numbers2) {
+            sum1.remove(number2);
+        }
+        System.out.println(sum1);
+
+        // B-A
+
+        HashSet<Integer> sum2 = new HashSet<>();
+
+        for (int number2 : numbers2) {
+            sum2.add(number2);
+        }
+        for (int number : numbers) {
+            sum2.remove(number);
+        }
+        System.out.println(sum2);
+
+        //Intercection
+//        for (int number : numbers) {
+//            sum.add(number);
+//        }
+//        for (int number2 : numbers2) {
+//            sum.add(number2);
+//        }
+        HashSet<Integer> inter1 = new HashSet<>();
+        
+        inter1 = sum.remove(sum1);
+        HashSet<Integer> inter2 = new HashSet<>();
+        inter2 = inter1.remove(sum2);
+
+        HashSet<Integer> sum3 = new HashSet<>();
+        sum3 = sum1.add(sum2);
+        System.out.println();
     }
 }
+
+
